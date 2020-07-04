8aa8bd84126b6cccf29a4b9067fe1dad56d5e107
Yura Frozen <63106222+yuuretsu@users.noreply.github.com>
2020-07-05 02:05:06 +0500

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,11 @@ pub fn normalize(p: Vec2i, s: Vec2i) -> Vec2i {
     Vec2i::new(x, y)
 }
 
-pub fn get_sum(vec: Vec2i, world: &Vec<Vec<bool>>) -> u8 {
+pub fn get_sum(vec: Vec2i, world: &Vec<bool>) -> u8 {
     let mut sum: u8 = 0;
     for i in 0..MOORE_NEIGHBORHOOD.len() {
         let point = normalize(vec + &MOORE_NEIGHBORHOOD[i], Vec2i::new(W as i32, H as i32));
-        sum += world[point.x as usize][point.y as usize] as u8;
+        sum += world[point.x as usize + point.y as usize * W] as u8;
     }
     sum
 }
@@ -54,31 +54,29 @@ pub const MOORE_NEIGHBORHOOD: [Vec2i; 8] = [
     Vec2i { x: -1, y:  0 },
 ];
 
-pub fn step(world: Vec<Vec<bool>>, mut world_next: Vec<Vec<bool>>) -> (Vec<Vec<bool>>, Vec<Vec<bool>>) {
+pub fn step(world: Vec<bool>, mut world_next: Vec<bool>) -> (Vec<bool>, Vec<bool>) {
     for x in 0..W {
         for y in 0..H {
-            let here = world[x][y];
+            let here = world[x + y * W];
             let sum = get_sum(Vec2i::new(x as i32, y as i32), &world);
             let next: bool;
             match here {
                 false => next = if sum == 3 {true} else {false},
                 true => next = if sum == 2 || sum == 3 {true} else {false},
             }
-            // println!("{}x{}", x, y);
-            // println!("{}", world_next[0].len());
-            world_next[x][y] = next;
+            world_next[x + y * W] = next;
         }
     }
     (world_next, world)
 }
 
-pub fn init_world() -> Vec<Vec<bool>> {
-    let mut world: Vec<Vec<bool>> = Vec::new();
+pub fn init_worlds() -> (Vec<bool>, Vec<bool>) {
+    let mut world = vec![false; W * H];
+    let world_next = vec![false; W * H];
     for x in 0..W {
-        world.push(Vec::new());
-        for _y in 0..H {
-            world[x].push(if rand_u8(0, 1) == 0 {false} else {true});
+        for y in 0..H {
+            world[x + y * W] = if rand_u8(0, 1) == 0 {false} else {true};
         }
     }
-    world
+    (world, world_next)
 }
